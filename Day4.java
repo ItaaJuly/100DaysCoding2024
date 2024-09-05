1d48bd614f92b4e42a9f44e000ba9f9c1fd8dee5
ItaaJuly <itaaataa4@gmail.com>
2024-09-05 08:37:47 +0800

    Membuat System.out.print dan System.out.printf

---
+++ Day4.java
@@ -0,0 +1,21 @@
+package itaa;
+
+public class Day004 {
+    public static void main(String[] args) {
+        
+        String a = "hai i'am ita";
+        String b = "hoby volly";
+        System.out.printf("%s %s",a,b);
+            
+        
+        
+        System.out.print("i'am ita");
+        System.out.print("fav color is black");
+        System.out.print("fav food is rice");
+        System.out.print("fav drink is ic tea");
+        
+            
+        }
+    
+        
+    
