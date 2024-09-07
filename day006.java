e5c2c88321695071d987b35854ae8bf60718bdc0
ItaaJuly <itaaataa4@gmail.com>
2024-09-07 22:14:34 +0800

    Membuat Variabel:Deklarasi

---
+++ day006.java
@@ -0,0 +1,16 @@
+package itaa;
+
+public class day6 {
+    public static void main(String[] args){
+        
+        String nama,kelas,fakultas;
+        nama = "ita";
+        kelas = "D";
+        fakultas = "teknk";
+        
+        System.out.println(nama);
+        System.out.println(kelas);
+        System.out.println(fakultas);
+        
+    }
+    
