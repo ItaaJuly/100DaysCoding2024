21ccaf088508cfcf6722c09b2e6830e53596c5de
ItaaJuly <itaaataa4@gmail.com>
2024-09-09 06:22:26 +0800

    Variabel:update

---
+++ day8.java
@@ -0,0 +1,15 @@
+package itaa;
+
+public class day8 {
+    public static void main(String[] args) {
+        // Update nilai variabel
+        int point = 8;
+        System.out.println("point awal:" +point);
+        point = 1;
+        System.out.println("total point :"+point);
+        
+        
+        
+        
+    }
+    
