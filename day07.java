db96d1017d9a9b41e97d9f69ad37bfacb7704af2
ItaaJuly <itaaataa4@gmail.com>
2024-09-08 09:04:59 +0800

    Variabel: inisialisasi

---
+++ day07.java
@@ -0,0 +1,20 @@
+package itaa;
+
+public class day7 {
+    public static void main(String[] args){
+    
+    // variabel inisialisasi
+        String name = "ita";
+        int age = 18;
+        double tb = 152.3;
+        boolean isActive = true;
+        
+        
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(tb);
+        System.out.println(isActive);
+        
+        
+            
+        }
