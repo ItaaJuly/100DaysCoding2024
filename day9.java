7d3552e90c8f7a069274f74cfe6862494e9f1e4c
ItaaJuly <itaaataa4@gmail.com>
2024-09-10 07:51:13 +0800

    Membuat Variabel:constanta

---
+++ day9.java
@@ -0,0 +1,17 @@
+package itaa;
+
+public class day9 {
+    public static void main(String[] args){
+        //Mendefinisikan konstanta
+        final int USIA = 18;
+        final String GREETING_MESSAGE = "Welcome to Java!";
+        final double TINGGI_BADAN = 152.3;
+        
+        // Menggunakan konstanta
+        System.out.println("int:" + USIA);
+        System.out.println("String:" + GREETING_MESSAGE);
+        System.out.println("double:" + TINGGI_BADAN);
+        
+        
+    }
+    
