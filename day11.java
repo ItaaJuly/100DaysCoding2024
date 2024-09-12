fe75fc487b75ea407369478ce00960c8de15ac68
ItaaJuly <itaaataa4@gmail.com>
2024-09-12 09:17:48 +0800

    Membuat tipe data:short

---
+++ day11.java
@@ -0,0 +1,12 @@
+package itaa;
+
+public class day11 {
+    public static void main(String[] args) {
+        //Membuat tipe data short
+        short a = 900;
+        short b = 1000;
+        short results = (short) (a + b);
+        
+        System.out.println("Results perjumlahan a dan b adalah: " + results );
+        
+    }
