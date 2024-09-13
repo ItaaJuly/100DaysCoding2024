a19d8d779db351f2b8471808a172ea6aacc7e53e
ItaaJuly <itaaataa4@gmail.com>
2024-09-14 07:05:02 +0800

    Tugas dari mentor

---
+++ day13.java
@@ -0,0 +1,16 @@
+package itaa;
+
+public class day13 {
+    public static void main(String[] args) {
+        // Variabel dengan tipedata integer
+        int age = 18; 
+        short year = 2006;
+        
+        System.out.println("this is my age:" + age);
+        
+        //Update
+        age = 19;
+        System.out.println("i'll be old next year:" + age);
+        System.out.printf("year:" + year);
+    }
+    
