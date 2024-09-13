b2a3d090059068157f33cb1d7e39c4ec5105e230
ItaaJuly <itaaataa4@gmail.com>
2024-09-13 08:31:30 +0800

    Membuat tipe data:int

---
+++ day12.java
@@ -0,0 +1,13 @@
+package itaa;
+
+public class day12 {
+    public static void main(String[] args) {
+        // Membuat varibel tipe data int
+        int age = 18;
+        int year = 2006;
+        
+        System.out.println("Age:" + age);
+        System.out.println("Year:" + year);
+        
+    }
+    
