093187b3736806a9878f601ca4f1c350d6616548
ItaaJuly <itaaataa4@gmail.com>
2024-09-15 18:35:39 +0800

    Membuat tipe data long

---
+++ day14.java
@@ -0,0 +1,11 @@
+package itaa;
+
+public class day14 {
+    public static void main(String[] args) {
+        //Membuat tipe data long
+        long notelfon = 622340101987L;
+        long norumah = 100;
+        System.out.println("Nilai notelfon: " + notelfon);
+        System.out.println("Norumah: " + norumah);
+    }
+    
