7aff34b918896533b6b6281ac2dbfff9328ef787
ItaaJuly <itaaataa4@gmail.com>
2024-09-16 21:33:23 +0800

    Membuat tipe data Double

--- day15.java
+++ day15.java
@@ -3,19 +3,10 @@ package itaa;
 public class day15 {
     public static void main(String[] args) {
         //Membuat tipedata double
-        double a = 2.576756675556;
-        double b = 2.311613663114;
-               //Operasa matematika
-                double sum = a + b;
-                double difference = a - b;
-                double product = a * b;
-                double quotient = a / b;
-        System.out.println("Nilai a: " + a);  
-        System.out.println("Nilai b: " + b);
-        System.out.println("Jumlah: " + sum);
-        System.out.println("Selisih: " + difference);
-        System.out.println("Produk: " + product);
-        System.out.println("Hasil bagi: " + quotient);        
-                
+        double tinggi = 152.20000000;
+        double berat = 39.40000000;
+             System.out.println("Tinggi saya: " + tinggi);
+             System.out.println("Berat saya: " + berat);
     }
+}
     
