e77baf1a1e0f37124e5dadc9842a8183deeabce4
ItaaJuly <itaaataa4@gmail.com>
2024-09-18 09:59:28 +0800

    Membuat tipedata char

---
+++ day17.java
@@ -0,0 +1,13 @@
+package itaa;
+
+public class day17 {
+    public static void main(String[] args) {
+        //Membuat tipe data char
+        char kelas = 'D';
+        char inisial = 'i';
+        System.out.println("Saya kelas: " + kelas);
+        System.out.println("inisial nama saya: " + inisial);
+        
+        
+    }
+    
