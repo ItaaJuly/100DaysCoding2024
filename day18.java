45edfc5e02a7467cdd13b5c32416c940e3c581c4
ItaaJuly <itaaataa4@gmail.com>
2024-09-19 18:32:33 +0800

    Membuat tipedata boolean

---
+++ day18.java
@@ -0,0 +1,12 @@
+package itaa;
+
+public class day18 {
+    public static void main(String[] args) {
+        //Membuat tipe data boolean
+        boolean suka = true;
+        boolean tidak = false;
+        
+        System.out.println("saya pinter > " + suka);
+        System.out.println("saya bdoh > " + tidak);
+    }
+    
