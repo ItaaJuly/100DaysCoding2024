9d988664468af6b9944ba6964828a5bbcc3a1c1b
ItaaJuly <itaaataa4@gmail.com>
2024-09-21 13:39:01 +0800

    Membuat tipedata reference: byte

---
+++ day20.java
@@ -0,0 +1,9 @@
+package itaa;
+
+public class day20 {
+    public static void main(String[] args) {
+        Byte a = 100;
+        Byte b = 50;
+        System.out.println("Nilai a:" + a);
+        System.out.println("Nilai b:" + b);
+    } 
