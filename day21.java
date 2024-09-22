24a92c210a404da27b0553bca07af9e067299ec1
ItaaJuly <itaaataa4@gmail.com>
2024-09-22 19:16:46 +0800

    Membuat tipedata reference: Short

---
+++ day21.java
@@ -0,0 +1,15 @@
+package itaa;
+
+
+public class day21 {
+    public static void main(String[] args) {
+      Short sod = 3;
+      Short anak_ke = 1;    
+        
+   System.out.println("Saya bersaudara:" + sod);
+   System.out.println("Saya anak ke:" + anak_ke);        
+        
+        
+    }
+    
+}
