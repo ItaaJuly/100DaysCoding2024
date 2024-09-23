a101cf0133a0042824db8f81d8190a66822e58c0
ItaaJuly <itaaataa4@gmail.com>
2024-09-23 13:39:19 +0800

    Membuat tipe data reference: Integer

---
+++ day22.java
@@ -0,0 +1,10 @@
+package itaa;
+
+public class day22 {
+    public static void main(String[] args) {
+        
+        Integer angkatan = 2024;
+        Integer selesai = 2027;
+        System.out.println("Masuk kuliah pada tahun:" + angkatan);
+        System.out.println("Dan semoga selesai pada tahun:" + selesai);
+    }
