e0a612dbb49cad0bbfdb01e174c44a8839dab165
ItaaJuly <itaaataa4@gmail.com>
2024-09-24 17:37:00 +0800

    Membuat tipe data reference:Long

---
+++ day23.java
@@ -0,0 +1,11 @@
+package itaa;
+
+public class day23 {
+    public static void main(String[] args) {
+        Long angka = 12345678910L;
+        Long Coding = 100L;
+        
+        System.out.println("Nomor itu ada 10 angka yaitu:" + angka);
+        System.out.println("Day coding" + Coding);
+    }
+    
