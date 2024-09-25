ef1efa830ab69ddbe3a18ad3d6927b6d07b75f26
ItaaJuly <itaaataa4@gmail.com>
2024-09-25 16:52:25 +0800

    Membuat tipe data reference: Float

---
+++ day24.java
@@ -0,0 +1,15 @@
+package Ita;
+
+
+public class java {
+     public static void main(String[] args) {
+         Float bb = 39.4F;
+         Float tb = 152.3F;
+         Float usia = 18.2F;
+         
+         System.out.println("Berat badan saya:" + bb);
+         System.out.println("Tinggi badan saya:" +tb);
+         System.out.println("Usia saya saat ini:" + usia);
+                
+    }
+}
