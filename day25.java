c0197623b9e1fd4f334b2756fe0b06ccd91832d2
ItaaJuly <itaaataa4@gmail.com>
2024-09-26 19:42:46 +0800

    Membuat tipe data reference:Double

---
+++ day25.java
@@ -0,0 +1,10 @@
+package itaaa;
+
+public class Dayduapuluhlima {
+    public static void main(String[] args) {
+        Double rok = 30.0;
+        Double sptu = 37.0;
+        System.out.println("Ukuran rok saya:" + rok);
+        System.out.println("Ukuran sepatu saya:" + sptu);
+    }
+    
