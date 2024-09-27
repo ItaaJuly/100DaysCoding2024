08cb52a90455c539af9feab89116d1a1a966f8ed
ItaaJuly <itaaataa4@gmail.com>
2024-09-27 11:08:25 +0800

    Membuat tipe data reference:Character

---
+++ day26.java
@@ -0,0 +1,10 @@
+package itaa;
+
+public class day26 {
+    public static void main(String[] args) {
+        Character a = 'i';
+        Character b = 't';
+        Character c = 'a';
+        System.out.print("Namaku:" + a+b+c);
+    }
+    
