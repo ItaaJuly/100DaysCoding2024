f70a01e40366c82853e24500025f78ef815be82e
ItaaJuly <itaaataa4@gmail.com>
2024-09-28 22:53:14 +0800

    Membuat soal evaluasi

---
+++ day27.java
@@ -0,0 +1,20 @@
+package Coding100day;
+
+
+
+public class Day27 {
+    public static void main(String[] args){
+        
+        System.out.println("selamat datang");
+        Integer jumlahKegiatan = 10;
+        final String namaKegiatan = "Pernikahan";
+        float durasiKegiatan = 3.5f;
+        //update
+        durasiKegiatan = 4.5f;
+        
+        System.out.printf("Kegaiatan %s memiliki durasi %.1f jam Jumlah Kegiatan yang direncanakan saat ini adalah %d",namaKegiatan,durasiKegiatan,jumlahKegiatan);
+                
+                
+              
+       
+    }
