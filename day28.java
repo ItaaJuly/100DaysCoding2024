21a499a40d45d76dce75cb53b9f57328283fb28d
ItaaJuly <itaaataa4@gmail.com>
2024-09-29 11:11:20 +0800

    Membuat tipe data reference:Boolean

---
+++ day28.java
@@ -0,0 +1,11 @@
+package itaa;
+
+public class day28 {
+    public static void main(String[] args) {
+        Boolean gagal = true;
+        Boolean berhasil = false;
+        
+        System.out.println("Gagal evaluasi:"+ gagal);
+        System.out.println("Berhasil evaluasi:"+ berhasil);
+    }
+    
