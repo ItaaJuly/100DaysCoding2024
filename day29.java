5831cf4ab5078300fb283b6416bb864274fecbe7
ItaaJuly <itaaataa4@gmail.com>
2024-09-30 20:22:16 +0800

    Membuat tipe data reference:BigInteger

---
+++ day29.java
@@ -0,0 +1,11 @@
+package itaa;
+
+import java.math.BigInteger;
+
+public class day29 {
+     public static void main(String[] args) {
+        BigInteger nomor1 = new BigInteger ("098765432112345678909786534201");
+        BigInteger nomor2 = new BigInteger ("00112233445566778899");
+        System.out.println("Ini adalah nomor ya ges ya:" + nomor1);
+        System.out.println("Inipun juga nomor ges haha:" + nomor2);  
+     }
