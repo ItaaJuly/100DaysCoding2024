abb42f4f38b0b779e3b321539f2aee5f953cccc1
ItaaJuly <itaaataa4@gmail.com>
2024-10-01 09:08:49 +0800

    Membuat tipe data reference:BigDecimal

---
+++ day30.java
@@ -0,0 +1,13 @@
+package itaa;
+
+import java.math.BigDecimal;
+
+public class Day30 {
+      public static void main(String[] args) {
+          BigDecimal angka = new BigDecimal ("123.456");
+          BigDecimal angkaa = new BigDecimal ("321.654");
+          
+          System.out.println("Nilai:"+ angka);
+          System.out.println("Nilai kebalikan:" + angkaa);
+      }
+    
