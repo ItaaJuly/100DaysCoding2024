8a1c3dd847fb02eeed64a966c3a56ac9eb763a86
ItaaJuly <itaaataa4@gmail.com>
2024-10-02 17:35:46 +0800

    Konversi String ke tipe data Primitif

---
+++ day31.java
@@ -0,0 +1,11 @@
+package itaa;
+
+
+public class day31 {
+    public static void main(String[] args) {
+        String strInt = "123";
+        int intValue = Integer.parseInt(strInt);
+        
+        System.out.println("Nanti dalam hitungan ketiga lompat semua yaa. Oke kita mulaii... "+ intValue + "  LOMPATTTTT!!!!!");
+    }
+    
