f2f0dc36f942eb52e547cbb1f36b7b14ebd71b12
ItaaJuly <itaaataa4@gmail.com>
2024-10-04 10:01:51 +0800

    Type casting

---
+++ day33.java
@@ -0,0 +1,17 @@
+package Day33;
+
+
+public class java {
+    public static void main(String[] args) {
+        //Type casting
+       int harga_bajuValue = 55;
+       double harga_sepatuValue = 100.5;
+       float harga_jilbabValue = 15.0f;
+       short harga_ciputValue = 10;
+       double jumlah = (double) harga_bajuValue + harga_sepatuValue + harga_jilbabValue + harga_ciputValue;
+          System.out.println("Hasil penjumlahan :" + jumlah);
+  
+       
+       
+    }
+    
