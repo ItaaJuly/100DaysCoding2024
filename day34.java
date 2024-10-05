6dbe4402910f726539d0aab0ad46ff0604149aec
ItaaJuly <itaaataa4@gmail.com>
2024-10-05 11:13:43 +0800

    Input dari keyboard

---
+++ day34.java
@@ -0,0 +1,22 @@
+package Ita;
+
+import java.util.Scanner;
+
+
+public class day34 {
+    public static void main(String[] args) {
+        Scanner ita = new Scanner (System.in);
+        //Input dari keyboard
+        
+        System.out.print("Nama:");
+        String nama = ita.nextLine();
+        
+        System.out.print("Umur:");
+        int umur = ita.nextInt();
+        
+        //Update
+        umur = ita.nextInt();
+        
+                
+    }
+    
