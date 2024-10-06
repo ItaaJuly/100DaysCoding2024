bea3110b7bb3adbf1dd1cc04a7007266e7884f4b
ItaaJuly <itaaataa4@gmail.com>
2024-10-06 11:19:21 +0800

    Operator Aritmatika: Penjumlahan & Pengurangan

---
+++ day35.java
@@ -0,0 +1,25 @@
+package Itaa;
+
+
+public class Day35 {
+    public static void main(String[] args) {
+        //Operator Aritmatika Penjumlahan & Pengurangan
+        //Penjumlahan
+        int a = 15;
+        byte b = 18;
+        short c = 19;
+        int jumlah = a+b+c;
+        System.out.printf("Umur saya dulu %d tahun dan umur saya sekarang %d tahun dan tahun depan %d tahun %n",a,b,c);
+        System.out.println("Jika dijumlah maka akan menghasilkan : " +(a+b+c));
+        int hasil_jumlah = 52;
+        
+        //Pengurangan
+        int d = 15;
+        byte e = 18;
+        short f = 19;
+       short kurang =(short) (a-b-c);
+        System.out.println("Nah jadi, tadi hasil penjumlahan ada 52 dan sekarang saya mau mengurang dengan umur dulu saya yaitu 15. ");
+        System.out.println("Umur dulu 15 tahun dan kurang dengan hasil jumlah yaitu 52 dan menghasilkan : " +(52-15));
+    }
+    
+    
