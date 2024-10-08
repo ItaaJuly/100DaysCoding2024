62b4ae2c4890dd0c9579ef7856d0409e53563fb9
ItaaJuly <itaaataa4@gmail.com>
2024-10-08 15:20:27 +0800

    Operator Penugasan: Pengisian Nilai, Penambahan Nilai dan Pengurangan Nilai

---
+++ day37.java
@@ -0,0 +1,23 @@
+package itaaa;
+
+public class day37 {
+    public static void main(String[] args) {
+       // Operator penugasan : Pengisian nilai,Penambahan dan Pengurangan
+       
+       //Pengisian nilai (=)
+       int nilai = 10;
+       nilai = 10;
+        System.out.println("Hasil pengisian : " + nilai);
+        
+        //Penambahan
+        
+        nilai += 10;
+        System.out.println("Hasil tambah : " + nilai);
+        
+        //Pengurangan
+       
+        nilai -= 10;
+        System.out.println("Hasil kurang : " + nilai);
+       
+    }
+    
