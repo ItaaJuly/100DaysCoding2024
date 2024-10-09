2eab599be8edcc9ddcd2774f0b13e0fd2d9cbc90
ItaaJuly <itaaataa4@gmail.com>
2024-10-09 19:21:07 +0800

    Operator Penugasan: Pengisian Nilai, Perkalian, Pengisian dan Pembagian

---
+++ day38.java
@@ -0,0 +1,23 @@
+package itaaa;
+
+
+public class Day38 {
+    public static void main(String[] args) {
+        //Operator Penugasan : Pengisian dan Perkalian, Pengisian dan Pembagian
+        
+        //Pengisian
+        int a= 20;
+        int b = 10;
+        System.out.printf("Nilai awal %d Nilai kedua %d %n",a,b);
+        
+        //Perkalian
+        a *= b;
+        System.out.println("Setelah perkalian nilai a :" + a);
+        
+        //Pembagian
+        b /= a;
+        System.out.println("Setelah pembagian nilai b :" + b);
+                
+       
+       
+    }
