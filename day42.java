9820302cf2eb31b982f407e1d9348a0cc9484f31
ItaaJuly <itaaataa4@gmail.com>
2024-10-13 11:12:25 +0800

    Operator Perbandingan: Sama dengan dan Tidak sama dengan

---
+++ day42.java
@@ -0,0 +1,20 @@
+package itaaa;
+
+
+public class Day42 {
+    public static void main(String[] args) {
+   
+        //Operator Perbandingan : Sama dengan dan tidak sama dengan
+        int a = 12;
+        int b = 4;
+        boolean hasil;
+        
+        hasil = a == b;
+        System.out.println(hasil);
+        
+        hasil = a != b;
+        System.out.println(hasil);
+        
+            
+    }
+    
