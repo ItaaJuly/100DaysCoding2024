382ac939f7c2e0dd623c33b6d9941e255885e256
ItaaJuly <itaaataa4@gmail.com>
2024-10-15 18:06:52 +0800

    Operator Logika:AND

---
+++ day44.java
@@ -0,0 +1,11 @@
+public class Day44 {
+    public static void main(String[] args) {
+        
+//        Operator Logika : AND
+              
+        boolean a = true;
+        boolean b = false;
+        System.out.println((2 > 1) && (1 < 2));
+
+                
+    }
