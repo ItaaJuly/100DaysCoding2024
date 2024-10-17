d246ee26e2544711913664342381fb32f41be2df
ItaaJuly <itaaataa4@gmail.com>
2024-10-17 20:50:54 +0800

    Operator Logika : Negasi/kebalikan

---
+++ day46.java
@@ -0,0 +1,16 @@
+package itaaa;
+
+import java.util.Scanner;
+
+public class Day46 {
+    public static void main(String[] args) {
+        Scanner ta = new Scanner(System.in);
+        //Operator Logika : Negasi/kebalikan
+        
+        boolean a = true;
+        boolean b = false;
+        System.out.println(!(2 > 5) && (5 > 2));
+            
+        }
+            
+        
