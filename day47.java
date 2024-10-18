3c157db05738c985f2eb92687aacd8eb25407f94
ItaaJuly <itaaataa4@gmail.com>
2024-10-18 20:04:54 +0800

    Percabangan:if

---
+++ day47.java
@@ -0,0 +1,19 @@
+package itaaa;
+
+import java.util.Scanner;
+
+public class Day47 {
+    public static void main(String[] args) {
+        Scanner ta = new Scanner (System.in);
+//        Percabangan : if
+         System.out.print("Besar A :");
+         int a = ta.nextInt();
+         System.out.print("Besar B :");
+         int b = ta.nextInt();
+         System.out.println("Jadi lebih besar a daripda b");
+         if (a > b) {
+             System.out.println("iya");
+        }
+                
+                
+    }
