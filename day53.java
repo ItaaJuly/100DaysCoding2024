49d64902f93f0f32feb392aa2f1cae6e2b27b7fc
ItaaJuly <itaaataa4@gmail.com>
2024-10-24 17:42:21 +0800

    Looping : For

---
+++ day53.java
@@ -0,0 +1,16 @@
+package itaaa;
+
+import java.util.Scanner;
+
+public class Day53 {
+    public static void main(String[] args) {
+        Scanner ta = new Scanner(System.in);
+        System.out.print("Masukkan penjumlahan : " );
+        int n = ta.nextInt();
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Masukkan nilai ke-" + (i + 1)+ " : ");
+            int nilai = ta.nextInt();
+           
+            }
+        }
+        
