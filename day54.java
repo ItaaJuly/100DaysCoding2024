3c9b86ce187d0f92fcc8ce39eda0fdaef4a44dda
ItaaJuly <itaaataa4@gmail.com>
2024-10-25 19:27:00 +0800

    Looping: Increment

---
+++ day54.java
@@ -0,0 +1,15 @@
+package itaaa;
+
+import java.util.Scanner;
+
+public class Day54 {
+    public static void main(String[] args) {
+        Scanner ta = new Scanner(System.in);
+        
+        System.out.print("Masukkan angka ganjil : ");
+        int a = ta.nextInt();
+        for (int nilai = 1; nilai <= 10; nilai+=2) {
+            System.out.println(nilai);
+            
+        }
+    }
