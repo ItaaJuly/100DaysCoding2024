57881792c5b9582d49b607f55d027df33e9ba812
ItaaJuly <itaaataa4@gmail.com>
2024-10-28 14:07:21 +0800

    Looping for decrement

--- day55.java
+++ day55.java
@@ -2,14 +2,13 @@ package itaaa;
 
 import java.util.Scanner;
 
-public class Day56 {
+public class Day55 {
     public static void main(String[] args) {
         Scanner ta = new Scanner(System.in);
-        //Looping Decrement
-        System.out.print("Masukkan nilai : ");
+        System.out.println("masukkan nilai: ");
         int a = ta.nextInt();
-        for (int go = a; go>= 1; go++) {
-            System.out.println(go);
+        for (int i = 0; i < i; i -=3) {
+            System.out.println("nilai: " + i);
         }
-                
-    }
+    }  
+}
