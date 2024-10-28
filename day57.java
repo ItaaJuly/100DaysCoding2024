30ef20b2dd4c3edddcf82c7657bebef4e8ee7fce
ItaaJuly <itaaataa4@gmail.com>
2024-10-28 13:52:07 +0800

    Looping while increment

--- day57.java
+++ day57.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class Day57 {
     public static void main(String[] args) {
         Scanner ta = new Scanner(System.in);
-        System.out.print("Masukkan suhu air: ");
-        int a = ta.nextInt();
-        if (a <= 0) {
-            System.out.println("Air membeku");
-        }else if (a > 0 && a <= 100) {
-            System.out.println("Air dalam keadaan cair");
-        }else if (a > 100) {
-            System.out.println("Air mendidih");
+        
+        //Looping while increment
+        System.out.print("Masukkan batas: ");
+        int batas = ta.nextInt();
+        
+        int a = 0;
+        while (a < batas) {
+            System.out.println("Nilai a:"  + a);
+           a++;
         }
                 
                 
     }
+
