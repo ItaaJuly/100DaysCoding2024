e584f6b84a35c1fe2f016c98f872b3b2eec70d63
ItaaJuly <itaaataa4@gmail.com>
2024-10-31 18:28:52 +0800

    Looping do while

---
+++ day60.java
@@ -0,0 +1,12 @@
+package itaaa;
+
+public class Day60 {
+    public static void main(String[] args) {
+        int no =  1;
+        do {
+            System.out.println("Angka saat ini: "+ no);
+            no++;
+        } while (no <= 5);
+        System.out.println("Selesai");
+                
+    }
