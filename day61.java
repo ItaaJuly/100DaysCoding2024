120888178e7bf70590aeb8918a003c45b176422c
ItaaJuly <itaaataa4@gmail.com>
2024-11-01 11:26:18 +0800

    Infinite loop

---
+++ day61.java
@@ -0,0 +1,14 @@
+package itaaa;
+
+import java.util.Scanner;
+
+public class Day61 {
+    public static void main(String[] args) {
+        Scanner ta = new Scanner(System.in);
+          //infinite loop
+          while (true) {            
+              System.out.println("Ini adalah infinite loop.");
+        }
+             
+              }
+        
