2a891e38310f89ad5cf858878fa4e0c3a9d243f1
ItaaJuly <itaaataa4@gmail.com>
2024-11-02 20:34:30 +0800

    Keyword break

---
+++ day62.java
@@ -0,0 +1,14 @@
+package itaaa;
+public class Day62 {
+    public static void main(String[] args) {
+        
+        //Keywoard break
+        for (int i = 0; i < 10; i++) {
+            if (i == 5) {
+                break;
+            }
+            System.out.println(i);
+        }
+        System.out.println("Loop dihentikan");
+    }
+    
