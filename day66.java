0a185fd06e6cbd9cc2fea4c508a5e0e2f0d6c9be
ItaaJuly <itaaataa4@gmail.com>
2024-11-06 18:20:27 +0800

    Pola persegi dan persegi panjang

---
+++ day66.java
@@ -0,0 +1,17 @@
+package itaaa;
+public class Day66 {
+    public static void main(String[] args) {
+        
+        //Pola persegi dan persegi panjang
+        int p = 5;
+        int l = 8;
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < l; j++) {
+                System.out.print("o ");
+            }
+            System.out.println();
+        }
+                
+    }
+    
+}
