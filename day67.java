6af745287d6e34a4553c4fdaa9119ce03d6749ea
ItaaJuly <itaaataa4@gmail.com>
2024-11-07 15:11:15 +0800

    Method Void

---
+++ day67.java
@@ -0,0 +1,10 @@
+package itaaa;
+public class Day67 {
+    
+    //Methode Void
+    public static void cetakPesan() {
+        System.out.println("Ini adalah mothode void");
+    }
+    public static void main(String[] args) {
+        cetakPesan();
+    }
