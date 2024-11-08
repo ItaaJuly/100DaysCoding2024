94ef4782414d0803b9469c573d1e668d26c456f5
ItaaJuly <itaaataa4@gmail.com>
2024-11-08 18:25:40 +0800

    Method non-void

---
+++ day68.java
@@ -0,0 +1,11 @@
+package itaaa;
+public class Day68 {
+    //Method non-void
+    public static void sapa(String nama){
+        System.out.println("Haii, " + nama + " ! ");
+    }
+    public static void main(String[] args) {
+        sapa("Itaa");
+    }
+    
+    
