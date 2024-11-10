81338eed8c4950d3b48ea274142367ade787cca6
ItaaJuly <itaaataa4@gmail.com>
2024-11-10 09:51:41 +0800

    Method parameter

---
+++ day70.java
@@ -0,0 +1,11 @@
+package itaaa;
+public class Day70 {
+        //Methode dengan parameter
+        public static void sapaan(String nama){
+            System.out.println("oii " + nama + "!");
+        }
+        public static void main(String[] args) {
+            //Memanggil methode dengan parameter
+        sapaan("itaa");
+    }
+    
