5e3ad3604bf10edd2ccb3b5296e02dacdc98170c
ItaaJuly <itaaataa4@gmail.com>
2024-11-11 18:38:46 +0800

    method static

---
+++ day71.java
@@ -0,0 +1,11 @@
+package itaaa;
+public class Day71 {
+    //Methode static
+    public static void hola(){
+        System.out.println("Ini met static");
+    }
+    public static void main(String[] args) {
+        //Memanggil method static tanpa objek
+        hola();
+    }
+    
