09dc56cac574f7002a426891b7bc116c832967c6
ItaaJuly <itaaataa4@gmail.com>
2024-11-13 20:33:10 +0800

    String method: charAt ()

---
+++ day73.java
@@ -0,0 +1,12 @@
+package itaaa;
+public class Day73 {
+    public static void main(String[] args) {
+        
+        //String method: charAt()
+        String str = "hai ita cantik";
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println("Karakter pada index" + i + ";" + str.charAt(i));
+        }
+       
+    }
+    
