9a7655e0a511420c9aa25847121dd75c83780d80
ItaaJuly <itaaataa4@gmail.com>
2024-11-14 21:01:11 +0800

    String method: length ()

---
+++ day74.java
@@ -0,0 +1,11 @@
+package itaaa;
+public class Day74 {
+    public static void main(String[] args) {
+        //String method: length()
+        //Mendeklarasikan stirng
+        String str = "satuu";
+        
+        //Menggunakan method length untuk mendapatkan panjang string
+        int pStr = str.length();
+        System.out.println("Panjang String: " + pStr);
+    }
