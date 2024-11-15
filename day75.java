4c4e3ced577899788f4e849e13fd1cbc9a9e2204
ItaaJuly <itaaataa4@gmail.com>
2024-11-15 09:09:55 +0800

    String method: toLowerCase ()

---
+++ day75.java
@@ -0,0 +1,14 @@
+package itaaa;
+public class Day75 {
+    public static void main(String[] args) {
+        //String method : toLowerCase
+        
+        String str = "Good Bye!!!";
+        
+        //Mengubah semua huruf menjadi huruf kecil
+        String lowerStr = str.toLowerCase();
+        
+        System.out.println("String asli: " + str);
+        System.out.println("Seelah diubah: " + lowerStr);
+    }
+    
