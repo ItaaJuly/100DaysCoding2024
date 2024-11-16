0997248e9dcf7f5c1ce704e3f1b401ce7852fbed
ItaaJuly <itaaataa4@gmail.com>
2024-11-17 07:41:32 +0800

    String method: equals ()

---
+++ day77.java
@@ -0,0 +1,15 @@
+package itaaa;
+public class Day77 {
+    public static void main(String[] args) {
+        //Stirng method: equals()
+        String str1 = "Ita";
+        String str2 = "Ita";
+        String str3 = "ita";
+        
+        //Membndigkan str1 dan str2
+        System.out.println(str1.equals(str2));
+        
+        //Membndingkan str1 dan str3
+        System.out.println(str1.equals(str3));
+    }
+    
