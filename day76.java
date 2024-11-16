39a6ab159d0f322498c74aae80f6ef06f8dc5afe
ItaaJuly <itaaataa4@gmail.com>
2024-11-16 11:28:08 +0800

    String method: toUpperCase ()

---
+++ day76.java
@@ -0,0 +1,11 @@
+package itaaa;
+public class Day76 {
+    public static void main(String[] args) {
+        //String method : toUpperCase()
+        
+        String a = "Semangat";
+        
+        //Mengubah string menjadi huruf besar semua
+        String b = a.toUpperCase();
+        System.out.println(b);
+    }
