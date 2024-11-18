90fe7dd0bbda4879c9dd24681a1e39ff95681dbd
ItaaJuly <itaaataa4@gmail.com>
2024-11-19 00:00:40 +0800

    Method to string

---
+++ day79.java
@@ -0,0 +1,9 @@
+package itaaa;
+public class Day79 {
+    public static void main(String[] args) {
+        //to string()
+        String  a = "Ngoding Yok";
+        String result = a.toString();
+        System.out.println(result);
+        
+    }
