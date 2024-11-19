ed67ce932c88251273da85ba3308978aaea8613c
ItaaJuly <itaaataa4@gmail.com>
2024-11-20 00:16:48 +0800

    Method trim

---
+++ day80.java
@@ -0,0 +1,10 @@
+package itaaa;
+public class Day80 {
+    public static void main(String[] args) {
+        //trim
+        String a = " Helo gess ";
+        //Menggunakan trim untuk menghapus spasi di awal dan di akhir
+        String trimmed = a.trim();
+        System.out.println("Sblum trim: '" + a + "'");
+        System.out.println("Stelah trim: '" + trimmed + "'");
+    }
