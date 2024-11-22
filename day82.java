ffa14b4b1a7f76ba512f40bb38d5f27c2488f424
ItaaJuly <itaaataa4@gmail.com>
2024-11-22 19:07:29 +0800

    Method ceil (), floor() dan round()

---
+++ day82.java
@@ -0,0 +1,22 @@
+package itaaa;
+public class Day82 {
+    public static void main(String[] args) {
+       // Method ceil()
+       double a = 5.2;
+       // Menggunakan ceil() untuk membulatkan ke atas
+       double hasilCeil = Math.ceil(a);
+        System.out.println("Ceil 5.2 jadi " + hasilCeil);
+        
+        //Method floor
+        double b = 5.7;
+        //Menggunakan floor() untuk membulatkan ke atas
+        double hasilFloor = Math.floor(b);
+        System.out.println("Floor 5.7 jadi " + hasilFloor);
+        
+        //Method round()
+        double c = 10.5;
+        //Ini untuk membulatkan angka
+        long hasilC = Math.round(c);
+        System.out.println("round 10.5 jadi" + hasilC);
+    }
+    
