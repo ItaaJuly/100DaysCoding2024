52a389f0a7038c8ed8862b5880cc2761da67c639
ItaaJuly <itaaataa4@gmail.com>
2024-11-23 18:19:53 +0800

    Array 1D

---
+++ day83.java
@@ -0,0 +1,10 @@
+package itaaa;
+public class Day83 {
+    public static void main(String[] args) {
+        // Array 1D
+        int[] angka = {10, 20, 30, 40, 50};
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("Elemen ke-" + (i + 1) + ": " + angka[i]);
+        }
+    }
+    
