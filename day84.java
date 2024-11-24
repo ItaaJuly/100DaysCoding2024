3dcfe9c75127c5bf0f0ba8abd8293c626b25844a
ItaaJuly <itaaataa4@gmail.com>
2024-11-24 22:59:26 +0800

    Evaluasi 3

---
+++ day84.java
@@ -0,0 +1,13 @@
+package itaaa;
+
+import java.util.Scanner;
+
+public class Day84 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        //EVALUSI BESAR 3
+        
+        String str = in.nextLine();
+        String lower = str.toLowerCase();
+        System.out.println(lower);
+    }
