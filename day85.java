a48845fc2b50eea6f579fd62dcc15193d591c3ff
ItaaJuly <itaaataa4@gmail.com>
2024-11-25 22:44:55 +0800

    Mengisi elemen ke Array satu dimensi pada saat di inisialisasi

---
+++ day85.java
@@ -0,0 +1,7 @@
+package itaaa;
+public class Day85 {
+     public static void main(String[] args) {
+         int[] angka = {2, 4, 6, 8, 10};
+         System.out.println("Elemen kedua: " + angka[1]);
+    }
+    
