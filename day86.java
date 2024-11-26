067872f1f1c3a681b7c0fa5ac5f7b0cf1bf4fec4
ItaaJuly <itaaataa4@gmail.com>
2024-11-26 19:30:59 +0800

    Mencetak elemen array

---
+++ day86.java
@@ -0,0 +1,13 @@
+package itaaa;
+public class Day86 {
+    public static void main(String[] args) {
+        //Deklarasi dan inisialisasi array
+        int[] angka = {10, 20, 30};
+        
+        //Mencetak elemen-elemen array
+        System.out.println("Elemen-elemen pada array: ");
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("Elemen ke- " + i +  ": " + angka[i]);
+        }
+    }
+    
