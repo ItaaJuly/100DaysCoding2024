178ada9280c881cfb4a71bdc3255997b8d0b1cb4
ItaaJuly <itaaataa4@gmail.com>
2024-12-04 18:51:49 +0800

    Pola segitiga

---
+++ day94.java
@@ -0,0 +1,12 @@
+package itaaa;
+public class Day94 {
+    public static void main(String[] args) {
+        
+        int a = 5;
+        for (int i = 0; i <= a; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("o ");
+            }
+            System.out.println();
+        }
+    }
