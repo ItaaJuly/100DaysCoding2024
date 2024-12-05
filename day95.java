c2f8cdb228847caf62ecce5c065564ca453d006d
ItaaJuly <itaaataa4@gmail.com>
2024-12-05 18:56:14 +0800

    Pattern segitiga siku dan sebaliknya

---
+++ day95.java
@@ -0,0 +1,12 @@
+int a = 0;
+        while (a < 5) { 
+            int b = 5;
+            while (b > a) {
+                b--;
+                System.out.print(" o ");
+            }
+            System.out.println(" ");
+            a++;
+        }
+       
+    
